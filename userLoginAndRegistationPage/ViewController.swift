420ac5a10b89d7948dde9fe5cc788bbdbcbd5721
student13 <student13@felix-itss-macbook-pro.local>
2019-09-09 13:32:42 +0530

    Initial Commit

---
+++ userLoginAndRegistationPage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  userLoginAndRegistationPage
+//
+//  Created by student13 on 09/09/19.
+//  Copyright © 2019 prajakta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
